23754a9411912524780cf2f4f420363bc890755a
Adam Moss <adam.moss@nottingham.ac.uk>
2019-05-10 11:30:09 +0100

    Wip

--- minimize.py
+++ minimize.py
@@ -7,27 +7,34 @@ import torch
 import numpy as np
 
 
-def optimise(num_particles, dim, lam, num_iters=5000, lr=0.1):
+def optimise(num_particles, dim, lam, num_iters=5000, lr=0.1, log_dir='', output_iter=100):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # Initialise random particle positions
     x = torch.randn(num_particles, dim, requires_grad=True, device=device)
 
     opt = torch.optim.Adam([x], lr=lr)
 
+    # Indices of upper triangular distance matrix
     idx = torch.triu(torch.ones(num_particles, num_particles), diagonal=1) == 1
+
+    # Create log directory if it doesn't exist
+    if log_dir and not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
     for i in range(num_iters):
         opt.zero_grad()
-        #d = torch.nn.functional.pdist(x)
-        d = torch.norm(x[:, None] - x, dim=2, p=2)[idx]
-        V = torch.sum(1 / d) + lam / 6 * torch.sum(torch.norm(x, dim=1))
+        # pdist is faster but gives a CUDA error for high num_particles
+        #dist = torch.nn.functional.pdist(x)
+        dist = torch.norm(x[:, None] - x, dim=2, p=2)[idx]
+        V = torch.sum(1 / dist) + lam / 6 * torch.sum(torch.norm(x, dim=1))
         V.backward()
         opt.step()
-        if i % 100 == 0:
+        if i % output_iter == 0:
             print('Iter [%i] V [%5.4f]' % (i, V.detach().cpu().numpy()))
-
-    return x.detach().cpu().numpy()
+            if log_dir:
+                np.savetxt(os.path.join(log_dir, 'positions.txt'), x.detach().cpu().numpy(), fmt='%1.4e')
 
 
 if __name__ == '__main__':
@@ -40,8 +47,4 @@ if __name__ == '__main__':
     parser.add_argument('--log_dir', type=str, default='logs/test')
     args = parser.parse_args()
 
-    x = optimise(args.particles, args.dim, args.lam, num_iters=args.iters)
-
-    if not os.path.exists(args.log_dir):
-        os.makedirs(args.log_dir)
-    np.savetxt(os.path.join(args.log_dir, 'positions.txt'), x)
+    optimise(args.particles, args.dim, args.lam, num_iters=args.iters, log_dir=args.log_dir)
