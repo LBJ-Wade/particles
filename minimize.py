1c5cbf898c9475a0326b8031c8c38279e592c226
Adam Moss <adam.moss@nottingham.ac.uk>
2019-05-31 15:29:51 +0100

    Variable particle masses

--- minimize.py
+++ minimize.py
@@ -7,14 +7,18 @@ import torch
 import numpy as np
 
 
-def pairwise_distances(x):
-    res = torch.norm(x[:, None] - x, dim=2, p=2)
+def pairwise_forces(x, masses):
+    mass_matrix = masses[:, None] * masses
+    res = torch.norm(x[:, None] - x, dim=2, p=2) * mass_matrix
     return res
 
 
-def optimise(num_particles, dim, lam, num_iters=5000, lr=0.1, log_dir='', output_iter=100):
+def optimise(masses, dim, lam, num_iters=5000, lr=0.1, log_dir='', output_iter=100):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    masses = torch.from_numpy(masses).float().to(device)
+    num_particles = masses.size()[0]
 
     # Initialise random particle positions
     x = torch.randn(num_particles, dim, requires_grad=True, device=device)
@@ -28,18 +32,19 @@ def optimise(num_particles, dim, lam, num_iters=5000, lr=0.1, log_dir='', output
     if log_dir and not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
+    if log_dir:
+        f = open(os.path.join(log_dir, 'log.txt'), 'w')
+
     for i in range(num_iters):
         opt.zero_grad()
-        # pdist is faster but gives a CUDA error for high num_particles
-        #dist = torch.nn.functional.pdist(x)
-        dist = pairwise_distances(x)[idx]
-        V = torch.sum(1 / dist) + lam / 6 * torch.sum(torch.norm(x, dim=1)**2)
+        dist = pairwise_forces(x, masses)[idx]
+        V = torch.sum(1 / dist) + lam / 6 * torch.sum(masses * torch.norm(x, dim=1)**2)
         V.backward()
         opt.step()
-        if i % output_iter == 0:
-            print('Iter [%i] V [%5.4f]' % (i, V.detach().cpu().numpy()))
-            if log_dir:
-                np.savetxt(os.path.join(log_dir, 'positions.txt'), x.detach().cpu().numpy(), fmt='%1.4e')
+        if i % output_iter == 0 and log_dir:
+            f.write('%i %5.4f \n' % (i, V.detach().cpu().numpy()))
+            f.flush()
+            np.savetxt(os.path.join(log_dir, 'positions.txt'), x.detach().cpu().numpy(), fmt='%1.4e')
 
 
 if __name__ == '__main__':
@@ -50,6 +55,12 @@ if __name__ == '__main__':
     parser.add_argument("--particles", type=int, default=100, help="Number of particles")
     parser.add_argument('--lam', type=float, default=3, help="Lambda")
     parser.add_argument('--log_dir', type=str, default='logs/test')
+    parser.add_argument('--masses', type=str)
     args = parser.parse_args()
 
-    optimise(args.particles, args.dim, args.lam, num_iters=args.iters, log_dir=args.log_dir)
+    if args.masses:
+        masses = np.loadtxt(args.masses)
+    else:
+        masses = np.ones(args.particles)
+
+    optimise(masses, args.dim, args.lam, num_iters=args.iters, log_dir=args.log_dir)
